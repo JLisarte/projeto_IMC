22caea1c11a888de9d3d345fa476e5ceb302337b
JLisarte <juliano.lisarte@gmail.com>
2023-12-14 16:20:05 -0300

    Refactor code structure and fix error handling

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
-import { Modal } from './modal.js'
-// variáveis 
-const form = document.querySelector('form');
-const inputWeight = document.querySelector('#weight');
-const inputHeight = document.querySelector('#height');
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { IMC, notNumber } from "./utils.js"
 
-form.onsubmit = e => {
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+form.onsubmit = (e) => {
   e.preventDefault()
 
   const weight = inputWeight.value
   const height = inputHeight.value
+
+  const showAlertError = notNumber(weight) || notNumber(height)
+
+  if (showAlertError) {
+    AlertError.open()
+    return
+  }
+
+  AlertError.close()
 
   const result = IMC(weight, height)
   const message = `Seu IMC é ${result}`
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
